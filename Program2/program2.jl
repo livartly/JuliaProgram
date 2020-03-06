4ba50d463055a5331d14e8f1a8ec55fc03af5a04
Liv Artly <37487751+livartly@users.noreply.github.com>
2020-03-06 02:26:58 -0600

    reading in file

--- Program2/program2.jl
+++ Program2/program2.jl
@@ -5,3 +5,19 @@ using Printf
 using Statistics
 startString = "Starting program..."
 println(startString)
+
+
+println("Enter file name")
+fileName = chomp(readline())
+println()
+
+fileData = String[]
+try
+    open(fileName,"r") do f
+        for line in eachline(f)
+            push!(fileData, line)
+        end
+    end
+catch
+    println("Error reading data")
+    exit(0)
