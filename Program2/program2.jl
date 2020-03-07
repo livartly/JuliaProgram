1093f3347cc626a58bb3b68581bc1b01a8c79ef7
Liv Artly <37487751+livartly@users.noreply.github.com>
2020-03-07 14:54:53 -0600

    Initialize player data into team struct

--- Program2/program2.jl
+++ Program2/program2.jl
@@ -20,17 +20,19 @@ end
 #****************
 # Structure/object to hold formatted player data
 #****************
-struct formattedPlayer
+struct formattedP
     firstName::String
     lastName::String
     average::Float64
     slug::Float64
     onBasePercent::Float64
+    team::Vector{Player}
 end
 
 #create data arrays
 fileData = String[]
 playerData = String[]
+playerReport = formattedP("", "", 0.0, 0.0, 0.0, Vector{Player}[])
 
 #open file
 open("julia.txt","r") do file
@@ -41,19 +43,26 @@ count = 0
     end
 
     #parse individual player data from fileData array
-    firstname = ""
     for i in 1:count
         playerData = split(fileData[i])
         firstname = playerData[1]
         lastname = playerData[2]
-        println(firstname)
-        println(lastname)
+        atbats = parse(Int32, playerData[3])
+        plateapp = parse(Int32, playerData[4])
+        singles = parse(Int32, playerData[5])
+        doubles = parse(Int32, playerData[6])
+        triples = parse(Int32, playerData[7])
+        homeruns = parse(Int32, playerData[8])
+        walks = parse(Int32, playerData[9])
+        hitbypitch = parse(Int32, playerData[10])
+
+        #add new player to
+        player = Player(firstname, lastname, atbats, plateapp, singles, doubles, triples, homeruns, walks, hitbypitch)
+        push!(playerReport.team, player)
     end
 end
 
-
-
-
+println(playerReport.team[1].homeRuns)
 
 #TODO: write sorting function
 #TODO: write various player statistics calculation
