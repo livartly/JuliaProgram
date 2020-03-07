c7167bd1ef7d742945345fb527106328dcc95795
Liv Artly <37487751+livartly@users.noreply.github.com>
2020-03-07 14:28:42 -0600

    Added test file + reading in file + setting names

--- Program2/program2.jl
+++ Program2/program2.jl
@@ -1,23 +1,65 @@
-#Programming Languages
-#Program 2
-
 using Printf
 using Statistics
-startString = "Starting program..."
-println(startString)
 
+#****************
+# Structure/Object to hold player data
+#****************
+struct Player
+    firstName::String
+    lastName::String
+    atBats::UInt64
+    plateApp::UInt64
+    singles::UInt64
+    doubles::UInt64
+    triples::UInt64
+    homeRuns::UInt64
+    walks::UInt64
+    hitPitch::UInt64
+end
 
-println("Enter file name")
-fileName = chomp(readline())
-println()
+#****************
+# Structure/object to hold formatted player data
+#****************
+struct formattedPlayer
+    firstName::String
+    lastName::String
+    average::Float64
+    slug::Float64
+    onBasePercent::Float64
+end
 
+#create data arrays
 fileData = String[]
-try
-    open(fileName,"r") do f
-        for line in eachline(f)
-            push!(fileData, line)
-        end
+playerData = String[]
+
+#open file
+open("julia.txt","r") do file
+count = 0
+    for line in eachline(file)
+        push!(fileData, line)
+        count += 1
     end
-catch
-    println("Error reading data")
-    exit(0)
+
+    #parse individual player data from fileData array
+    firstname = ""
+    for i in 1:count
+        playerData = split(fileData[i])
+        firstname = playerData[1]
+        lastname = playerData[2]
+        println(firstname)
+        println(lastname)
+    end
+end
+
+
+
+
+
+#TODO: write sorting function
+#TODO: write various player statistics calculation
+#TODO: calculate player averages
+#TODO: write function to return the path of the input file
+#TODO: write function to read in input file
+#TODO: write function to parse data file line by line
+#TODO: write function to format final player report
+#TODO: write main driver function
